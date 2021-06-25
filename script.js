3bee3eec00fd2edc76b7aa8f7a1f5d7e0b3e7c18
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-06-25 16:27:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// DEEL A: zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+ 
+ikRockArrowFunctions()
+
+// DEEL B
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven()
+
+// DEEL C
+const myFunction = (a, b) => a + b;
+
+myFunction(3, 5);
+
+
+// DEEL D
+const addFive = a => a + 5;
+
+addFive(6);
+
+// DEEL E
+const createObject = () => ({greeting: "hoi"});
